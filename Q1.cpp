b76aa8cd0e2332ffb2d4a19982039bd73ed80ca4
irzam-wk <107143716+irzam-wk@users.noreply.github.com>
2023-07-27 23:59:10 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -5,17 +5,20 @@ int lengthOf(char* firstChar){}*/
 using namespace std;
 int lengthOf(char *firstChar){
     printf("Length of string is ");
-    string l = firstChar;
-    int i = 0;
-    cout<<l.size();
-    return 0;
+    char *ptr = firstChar;
+    int j = 0;
+    while(*ptr!='\0'){
+        ptr++;
+        j++;
+    }
+    return j;
 }
 int main() {
     char fChar;
-    string str;
+    char str[25];
     cout<<"ENter string\n";
     cin>>str;
     fChar = str[0];
-    lengthOf(&fChar);
+    cout<<lengthOf(&str[0]);
     return 0;
 }
